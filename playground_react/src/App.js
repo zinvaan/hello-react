2593a4c8d92bde246e6d82ed5f26df695f74b0b8
Your Name <dnjswls0506@naver.com>
2022-02-13 22:32:37 +0900

    리액트(객체 선언 없이 style값 지정하기)

--- playground_react/src/App.js
+++ playground_react/src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 function App() {
   const name = '리액트';
-  const style = {
-    // 카멜표기법으로 작성해야 한다.
-    backgroundColor: 'black', // background-color를 camelCase로 표기
-    color: 'aqua',
-    fontSize: '48px', // font-size를 camelCase로 표기
-    fontWeight: 'bold', // font-weigth를 camelCase로 표기
-    padding: 16 // 단위 생략하면 px로 지정된다.
-  };
-  return <div style={style}>{name}</div>;
+  return(
+    <div style={{
+      // 카멜표기법으로 작성해야 한다.
+      backgroundColor: 'black', // background-color를 camelCase로 표기
+      color: 'aqua',
+      fontSize: '48px', // font-size를 camelCase로 표기
+      fontWeight: 'bold', // font-weigth를 camelCase로 표기
+      padding: 16 // 단위 생략하면 px로 지정된다.
+    }}> {name} </div>
+  )
+
+  // const style = {
+  //   // 카멜표기법으로 작성해야 한다.
+  //   backgroundColor: 'black', // background-color를 camelCase로 표기
+  //   color: 'aqua',
+  //   fontSize: '48px', // font-size를 camelCase로 표기
+  //   fontWeight: 'bold', // font-weigth를 camelCase로 표기
+  //   padding: 16 // 단위 생략하면 px로 지정된다.
+  // };
+  // return <div style={style}>{name}</div>;
 }
 
 export default App;
