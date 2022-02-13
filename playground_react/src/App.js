b9a2689f7e6ecdce31a0e500e23705f834f008d6
Your Name <dnjswls0506@naver.com>
2022-02-13 22:21:57 +0900

    리액트(undefined 일 때 보여 주고 싶은 문구 표현법)

--- playground_react/src/App.js
+++ playground_react/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
 function App() {
-  const name = "리웩트";
-  return(
-    <div>
-      {name === '리액트' && <h1>리액트 입니다.</h1>}
-    </div>
-  )
+  const name = undefined;
+  return name || '값이 undefiend 입니다.';
 }
 
 export default App;
