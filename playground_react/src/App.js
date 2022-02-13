88cf6d551d666b2acfc7ea50e1c2cce4ef1b5fb1
Your Name <dnjswls0506@naver.com>
2022-02-13 22:04:07 +0900

    리액트(AND연산자를 사용한 조건부 렌더링)

--- playground_react/src/App.js
+++ playground_react/src/App.js
@@ -1,12 +1,10 @@
-import React, {Fragment} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  const name = "리액트";
+  const name = "리웩트";
   return(
     <div>
-      {name === '리액트'?(<h1>리액트입니다.</h1>):(<h2>리액트가 아닙니다.</h2>)};
+      {name === '리액트' && <h1>리액트 입니다.</h1>}
     </div>
   )
 }
