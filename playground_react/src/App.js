e166e218120fff38a801713e6ac384ca7eca5e2c
Your Name <dnjswls0506@naver.com>
2022-02-13 21:59:32 +0900

    리액트(조건부연산자 사용하기)

--- playground_react/src/App.js
+++ playground_react/src/App.js
@@ -1,25 +1,14 @@
+import React, {Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const name = "리액트";
+  return(
+    <div>
+      {name === '리액트'?(<h1>리액트입니다.</h1>):(<h2>리액트가 아닙니다.</h2>)};
     </div>
-  );
+  )
 }
 
 export default App;
