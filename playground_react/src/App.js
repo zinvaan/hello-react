093a0ce91dba4319370021d7ac47c0ee28717dfd
Your Name <dnjswls0506@naver.com>
2022-02-13 22:28:34 +0900

    리액트(인라인 스타일링 방법)

--- playground_react/src/App.js
+++ playground_react/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function App() {
-  const name = undefined;
-  return name || '값이 undefiend 입니다.';
+  const name = '리액트';
+  const style = {
+    // 카멜표기법으로 작성해야 한다.
+    backgroundColor: 'black', // background-color를 camelCase로 표기
+    color: 'aqua',
+    fontSize: '48px', // font-size를 camelCase로 표기
+    fontWeight: 'bold', // font-weigth를 camelCase로 표기
+    padding: 16 // 단위 생략하면 px로 지정된다.
+  };
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
