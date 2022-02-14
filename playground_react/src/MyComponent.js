6da4898e25b1dedb0447fd3fc50bbe39e205b8d4
Your Name <dnjswls0506@naver.com>
2022-02-15 00:42:19 +0900

    리액트(필수 propTyes 설정하기: isRequired)

--- playground_react/src/MyComponent.js
+++ playground_react/src/MyComponent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types'; //props의 타입을 지정하기: propTypes를 사용
 
-const MyComponent = ({name, children}) => { 
+const MyComponent = ({name, favoriteNumber, children}) => { 
     //함수형 컴포넌트에서 props사용 할 때
     //파라미터 부분에서 비구조화 할당 문법 사용.
     return (
     <div>
         안녕하세요. 제 이름은 {name}입니다.<br/>
-        children 값은 {children}입니다.
+        children 값은 {children}입니다.<br/>
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>);
 };
 
@@ -16,7 +17,8 @@ MyComponent.defaultProps ={
 };
 
 MyComponent.propTypes = {
-    name: PropTypes.string //name 값이 문자열 형태로 전달되어야 함을 의미.
+    name: PropTypes.string, //name 값이 문자열 형태로 전달되어야 함을 의미.
+    favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
