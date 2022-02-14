0e4d1bb599868183c7ad9d8224ccb7793b0cc3b8
Your Name <dnjswls0506@naver.com>
2022-02-14 23:20:14 +0900

    리액트(App컴포넌트에서 MyComponent의 props값 지정하기)

--- playground_react/src/MyComponent.js
+++ playground_react/src/MyComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const MyComponent = () => { //function 키워드 대신 화살표 함수 사용
-    return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = props => { //props 값은 컴포넌트 함수의 파라미터로 받아와 사용할 수 있다.
+    return <div>안녕하세요. 제 이름은 {props.name}입니다.</div>;
 };
 
 export default MyComponent;
