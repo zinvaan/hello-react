20708acc552f6ddab8986fbf6186bb9bd194f0f6
Your Name <dnjswls0506@naver.com>
2022-02-15 00:51:27 +0900

    리액트(클래스형 컴포넌트에서 props 사용하기)

--- playground_react/src/MyComponent.js
+++ playground_react/src/MyComponent.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types'; //props의 타입을 지정하기: propTypes를 사용
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'; 
 
-const MyComponent = ({name, favoriteNumber, children}) => { 
-    //함수형 컴포넌트에서 props사용 할 때
-    //파라미터 부분에서 비구조화 할당 문법 사용.
-    return (
-    <div>
-        안녕하세요. 제 이름은 {name}입니다.<br/>
-        children 값은 {children}입니다.<br/>
-        제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>);
-};
+class MyComponent extends Component {
+    render(){
+        const {name, favoriteNumber, children} = this.props; //비구조화 할당
+        return (
+            <div>
+                안녕하세요. 제 이름은 {name}입니다.<br/>
+                children 값은 {children}입니다.<br/>
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>);
+    }
+}
 
 MyComponent.defaultProps ={
     name: '기본 이름'
