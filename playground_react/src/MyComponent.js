1f4cced41fc94e6f72b37380808ada73ead173aa
Your Name <dnjswls0506@naver.com>
2022-02-14 23:43:37 +0900

    리액트(태그 사이의 내용을 보여주기: props.children)

--- playground_react/src/MyComponent.js
+++ playground_react/src/MyComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const MyComponent = props => { //props 값은 컴포넌트 함수의 파라미터로 받아와 사용할 수 있다.
-    return <div>안녕하세요. 제 이름은 {props.name}입니다.</div>;
+    return (
+    <div>
+        안녕하세요. 제 이름은 {props.name}입니다.<br/>
+        children 값은 {props.children}입니다.
+    </div>);
 };
 
 MyComponent.defaultProps ={
