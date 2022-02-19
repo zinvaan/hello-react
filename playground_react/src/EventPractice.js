0a8be0f1e4020c29f28b2f11644256e6a842623a
Your Name <dnjswls0506@naver.com>
2022-02-19 23:20:13 +0900

    리액트(useState를 통해 사용하는 상태에 문자열 대신 객체 넣어보기)

--- playground_react/src/EventPractice.js
+++ playground_react/src/EventPractice.js
@@ -1,37 +1,46 @@
 import React, {useState} from 'react';
 
 const EventPractice=()=>{
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername=(event)=>setUsername(event.target.value);
-    const onChangeMessage=(event)=>setMessage(event.target.value);
+    const [form, setForm] = useState({
+        username: '',
+        message: '',
+    });
+    const {username, message} = form;
+    const onChange =(event)=>{
+        const nextForm ={
+            ...form, //기존의 form 내용을 이 자리에 복사한 뒤
+            [event.target.name]: event.target.value, //원하는 값 덮어 씌우기
+        };
+        setForm(nextForm);
+    };
     const onClick=()=>{
         alert(username + ': ' + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: '',
+        });
     };
     const onKeyPress=(event)=>{
         if(event.key === 'Enter'){
             onClick();
         }
     };
-
     return(
         <div>
             <h1>이벤트 연습</h1>
             <input
-            type="text"
-            name="username"
+            type='text'
+            name='username'
             placeholder='사용자명'
             value={username}
-            onChange={onChangeUsername}
+            onChange={onChange}
             />
             <input
             type='text'
             name='message'
             placeholder='아무거나 입력해 보세요.'
             value={message}
-            onChange={onChangeMessage}
+            onChange={onChange}
             onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
