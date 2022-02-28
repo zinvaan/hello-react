de2227a0f78ceda9abbedb0ca3fe13a3f26e3fa6
Your Name <dnjswls0506@naver.com>
2022-03-01 01:13:44 +0900

    리액트(useReducer 사용하기1)

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,17 +1,9 @@
 import React, {useState} from 'react';
-import Info from './Info';
+import Counter from './Counter';
 
 const App=()=>{
-  const [visible, setVisible] = useState(false);
-
   return(
-    <div>
-      <button onClick={()=>{
-        setVisible(!visible);
-      }}>{visible?'보이기':'숨기기'}</button>
-      <hr/>
-      {visible && <Info/>}
-    </div>
+    <Counter/>
   );
 };
 
