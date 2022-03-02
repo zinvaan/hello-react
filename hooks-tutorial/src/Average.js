090851bdbb92841228bde42b1de4fcf0d8cf7780
Your Name <dnjswls0506@naver.com>
2022-03-02 22:55:38 +0900

    리액트(useCallback 사용하기)

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react';
+import React, {useState, useMemo, useCallback} from 'react';
 
 const getAverage=(numbers)=>{
     console.log('평균값 계산 중...');
@@ -13,15 +13,15 @@ const Average=()=>{
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange=(event)=>{
+    const onChange = useCallback((event)=>{
         setNumber(event.target.value);
-    };
+    }, []); //컴포넌트가 처음 렌더링 될 때만 함수 생성
 
-    const onInsert=(event)=>{
+    const onInsert= useCallback((event)=>{
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
 
     const avg=useMemo(()=>getAverage(list),[list]);
 
