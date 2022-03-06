9384650a4a5cc5d3e35d5a1354311d852076885d
Your Name <dnjswls0506@naver.com>
2022-03-06 22:49:29 +0900

    리액트(todo App 만들기5: 항목 추가 기능 구현하기)

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React,{useState, useCallback} from 'react';
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert=()=>{
+const TodoInsert=({onInsert})=>{
+    const [value, setValue]=useState('');
+
+    const onChange=useCallback((event)=>{
+        setValue(event.target.value);
+    },[]);
+
+    const onSubmit=useCallback(
+        (event)=>{
+            onInsert(value);
+            setValue(''); //value 값 초기화
+
+            //submit 이벤트는 브라우저에서 새로고침을 발생시킨다.
+            //이를 방지하기 위해 이 함수를 호출.
+            event.preventDefault();
+        },[onInsert, value],
+    );
+
     return(
-        <form className='TodoInsert'>
-            <input placeholder='할 일을 입력하세요'/>
+        <form className='TodoInsert' onSubmit={onSubmit}>
+            <input placeholder='할 일을 입력하세요'
+            value={value}
+            onChange={onChange}
+            />
             <button type='submit'>
                 <MdAdd/>
             </button>
