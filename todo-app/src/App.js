8330fb36ad760c25ef6e36bfeff6d92b7590227e
Your Name <dnjswls0506@naver.com>
2022-03-06 23:29:27 +0900

    리액트(todo App 만들기6: 지우기 기능 추가하기)

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -35,11 +35,18 @@ const App=()=>{
       setTodos(todos.concat(todo));
       nextId.current += 1; //nextId 1씩 더하기
     },[todos],
-  );
+    );
+    
+    const onRemove=useCallback(
+      (id)=>{
+        setTodos(todos.filter((todo)=>todo.id !== id));
+      },[todos],
+    );
+
   return (
   <TodoTemplate>
     <TodoInsert onInsert={onInsert}/>
-    <TodoList todos={todos}/>
+    <TodoList todos={todos} onRemove={onRemove}/>
   </TodoTemplate>
   );
 };
