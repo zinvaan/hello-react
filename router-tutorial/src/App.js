dd38be85472ad617bb8beb2f94e8b842e962c61a
Your Name <dnjswls0506@naver.com>
2022-03-11 21:28:10 +0900

    리액트(리액트라우터 부가기능: history)

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App=()=>{
   return(
     <div>
-      <Route path='/' component={Home}/>
-      <Route path='/about' component={About}/>
+      <ul>
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to='/about'>소개</Link>
+        </li>
+        <li>
+          <Link to='/profiles'>프로필</Link>
+        </li>
+        <li>
+          <Link to='/history'>History 예제</Link>
+        </li>
+      </ul>
+      <Route path='/' component={Home} exact={true}/>
+      <Route path={['/about', '/info']} component={About}/>
+      <Route path='/profiles' component={Profiles}/>
+      <Route path='/history' component={HistorySample}/>
     </div>
   );
 };
