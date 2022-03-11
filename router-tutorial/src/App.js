e0ddadda676722d87493f08f13f2d695d68bbb08
Your Name <dnjswls0506@naver.com>
2022-03-11 17:47:51 +0900

    리액트(react-router-dom 버전 변경)

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import {  BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 const App=()=>{
   return(
     <div>
-      <ul>
-        <li>
-          <Link to ='/'>홈</Link>
-        </li>
-        <li>
-          <Link to='/about'>소개</Link>
-        </li>
-      </ul>
-      <hr/>
-      <Routes>
-       <Route path='/' element={<Home />}></Route>
-       <Route path='/about' element={<About />}></Route>
-       <Route path='/info' element={<About />}></Route>
-      </Routes>
+      <Route path='/' component={Home}/>
+      <Route path='/about' component={About}/>
     </div>
   );
 };
