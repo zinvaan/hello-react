8c5e59ac52fac2ef82c99e70bc88091a673b2231
Your Name <dnjswls0506@naver.com>
2022-03-13 20:46:17 +0900

    리액트(뉴스뷰어UI만들기)

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,21 +1,8 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App=()=>{
-  const [data, setData]=useState(null);
-  const onClick=()=>{
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response=>{
-      setData(response.data);
-    });
-  };
-  return(
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-    </div>
-  );
+  return <NewsList/>;
 };
 
 export default App;
