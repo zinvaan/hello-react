e9a428c97a22ba5838978695f2d0e46d088d8512
Your Name <dnjswls0506@naver.com>
2022-03-17 00:01:28 +0900

    리액트(동적 Context 사용하기)

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ColorBox from './components/ColorBox';
+import { ColorProvider } from './contexts/color';
+
+const App=()=>{
+  return(
+    <ColorProvider>
+    <div>
+      <ColorBox/>
+    </div>
+    </ColorProvider>
+  );
+};
+
+export default App;
