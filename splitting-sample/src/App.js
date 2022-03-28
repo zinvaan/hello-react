03a71ecfdc26698b1572e974383d92baf566f8a4
Your Name <dnjswls0506@naver.com>
2022-03-28 23:02:12 +0900

    리액트(React.lazy, Suspense를 통한 컴포넌트 코드 스플리팅)

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const SplitMe = React.lazy(()=>import('./SplitMe.js'));
+
 function App(){
+  const [visible, setVisible] = useState(false);
+  
   const onClick=()=>{
-    import('./notify').then(result=>result.default());
+    setVisible(true);
   };
 
   return(
@@ -12,6 +16,9 @@ function App(){
       <header className='App-header'>
         <img src={logo} className="App-logo" alt="logo"/>
         <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe/>}
+        </Suspense>
       </header>
     </div>
   );
