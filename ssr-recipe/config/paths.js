5327e8deaafa4cc644b83ab16497e2a9d3b8a60f
Your Name <dnjswls0506@naver.com>
2022-04-01 23:47:47 +0900

    리액트(서버 사이드 렌더링용 엔트리 만들기)

--- ssr-recipe/config/paths.js
+++ ssr-recipe/config/paths.js
@@ -70,6 +70,8 @@ module.exports = {
   appTsBuildInfoFile: resolveApp('node_modules/.cache/tsconfig.tsbuildinfo'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
   publicUrlOrPath,
+  ssrIndexJs: resolveApp('src/index.server.js'),
+  ssrBuild: resolveApp('dist'),
 };
 
 
