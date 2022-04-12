f31dea4a8a4aaeec70e4e5fc75ff3de83288aafd
Your Name <dnjswls0506@naver.com>
2022-04-12 22:33:52 +0900

    리액트(create 만들기)

--- react_practice2/my_app/src/App.js
+++ react_practice2/my_app/src/App.js
@@ -12,7 +12,6 @@ function Header(props){
     </header>
   );
 }
-
 function Nav(props){
   const lis = []
   for(let i=0; i<props.topics.length;i++){
@@ -38,7 +37,6 @@ function Nav(props){
     </nav>
   );
 }
-
 function Article(props){
   return(
     <article>
@@ -47,14 +45,32 @@ function Article(props){
     </article>
   );
 }
+function Create(props){
+  return(
+    <article>
+      <h2>Create</h2>
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder="title"></input></p>
+        <p><textarea name='body' placeholder='body'></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  );
+}
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'Javascript', body:'Javascript is ...'},
-  ]
+  ]);
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Read" body="Hello, Read"></Article>  
@@ -70,6 +86,22 @@ function App() {
     }
     content = <Article title={title} body={body}></Article>
   }
+  else if (mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id: nextId, title: _title, body: _body}
+      //상태를 다룰 때 그 상태가 객체와 같이 복합적인 데이터인 경우
+      //처리할 때 아래의 절차를 따른다
+      //1. 기존데이터를 복사하고 [...value];
+      const newTopics = [...topics];
+      //2. 복사한 데이터를 변경한 뒤
+      newTopics.push(newTopic);
+      //3. 변경한 데이터를 set 한다.
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
+  }
   return(
     <div>
       <Header title="WEB" onChangeMode={()=>{
@@ -80,6 +112,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
