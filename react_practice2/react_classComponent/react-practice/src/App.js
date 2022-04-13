037f7689b05d7105c2840a0c54fe918e2733c746
zinvaan <wonjin89jung@gmail.com>
2022-04-13 15:19:14 +0900

    리액트(state, props)

--- react_practice2/react_classComponent/react-practice/src/App.js
+++ react_practice2/react_classComponent/react-practice/src/App.js
@@ -5,12 +5,27 @@ import Content from './components/Content';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject: {title: 'WEB', sub: 'World Wide Web!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
   render(){
     return(
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="REACT" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC
+          data={this.state.contents}
+        ></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
