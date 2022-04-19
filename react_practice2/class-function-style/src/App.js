414fb1465005ceff6642595824232fd5b59de3d5
zinvaan <wonjin89jung@gmail.com>
2022-04-19 14:44:09 +0900

    리액트(함수형 컴포넌트에서 state 사용)

--- react_practice2/class-function-style/src/App.js
+++ react_practice2/class-function-style/src/App.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp(){
+function FuncComp(props){
+  let numberState = useState(props.initNumber);
+  let number = numberState[0];
+  let setNumber = numberState[1];
+
+  // let dateState = useState((new Date()).toString());
+  // let _date = dateState[0];
+  // let setDate = dateState[1];
+
+  let [_date, setDate] = useState((new Date()).toString());
+
   return(
     <div className="container">
       <h1>function style component</h1>
+      <p>Number: {number}</p>
+      <p>Date: {_date}</p>
+      <input type="button" value="random" onClick={
+          function(){
+            setDate((new Date).toString());
+          }
+        } ></input>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
+  state={
+    number: this.props.initNumber,
+    date: (new Date()).toString(),
+  }
   render(){
     return(
-      <div className="container" >
+      <div className="container">
         <h2>class style component</h2>
+        <p>Number: {this.state.number} </p>
+        <p>Date: {this.state.date} </p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number: Math.random()})
+          }.bind(this)
+        } ></input>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date: (new Date()).toString()})
+          }.bind(this)
+        } ></input>
       </div>
     )
   }
