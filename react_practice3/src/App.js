1dd968f84cb938074399144aa400ea50ae686f43
Your Name <dnjswls0506@naver.com>
2022-04-23 23:45:33 +0900

    리액트(오타 수정)

--- react_practice3/src/App.js
+++ react_practice3/src/App.js
@@ -5,9 +5,9 @@ import TodoList from './components/TodoList';
 
 const App=()=>{
   const [todos, setTodos] = useState([
-    {id:1, title:'리액트의 기초 알아보기', checked: true},
-    {id:2, title:'컴포넌트 스타일링 해보기', checked: true},
-    {id:3, title:'일정 관리 앱 만들어 보기', checked: false},
+    {id:1, text:'리액트의 기초 알아보기', checked: true},
+    {id:2, text:'컴포넌트 스타일링 해보기', checked: true},
+    {id:3, text:'일정 관리 앱 만들어 보기', checked: false},
   ]);
   return(
     <TodoTemplate>
