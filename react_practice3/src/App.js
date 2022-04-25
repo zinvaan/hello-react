58d584ea12c8e431f7eaeb15305a48e22a2c29f0
zinvaan <wonjin89jung@gmail.com>
2022-04-25 14:35:18 +0900

    리액트(TodoListItem에서 삭제 함수 만들기)

--- react_practice3/src/App.js
+++ react_practice3/src/App.js
@@ -20,11 +20,13 @@ const App=()=>{
     setTodos(todos.concat(todo));
     nextId.current += 1;
   },[todos]);
-
+  const onRemove = useCallback((id)=>{
+    setTodos(todos.filter((todo)=>todo.id !== id))
+  },[todos]);
   return(
     <TodoTemplate>
       <TodoInsert onInsert={onInsert}/>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} onRemove={onRemove}/>
     </TodoTemplate>
   );
 };
