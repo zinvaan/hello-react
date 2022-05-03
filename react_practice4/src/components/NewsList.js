507759a884af2f049fcbcb36b8d95b88bb6d8e3d
zinvaan <wonjin89jung@gmail.com>
2022-05-03 22:09:49 +0900

    리액트(API)

--- react_practice4/src/components/NewsList.js
+++ react_practice4/src/components/NewsList.js
@@ -1,9 +1,10 @@
 // API를 요청하고 뉴스 데이터가 들어 있는
 // 배열을 컴포넌트 배열로 반환하여 렌더링해주는 컴포넌트
 
-import React from "react";
+import React , {useState, useEffect} from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
 box-sizing: border-box;
@@ -26,14 +27,41 @@ const sampleArticle={
 };
 
 const NewsList=()=>{
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async()=>{
+      setLoading(true);
+      try{
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=dc443e0f23e048bbb6fb6c7232281b3b',
+        );
+        setArticles(response.data.articles);
+      }
+      catch(event){
+        console.log(event);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  },[]);
+
+  //대기 중 일 때
+  if(loading){
+    return <NewsListBlock>대기 중...</NewsListBlock>
+  }
+  //아직 articles 값이 설정되지 않았을 때
+  if(!articles){
+    return null;
+  }
+  // articles 값이 유효 할 때
   return(
     <NewsListBlock>
-      <NewsItem article={sampleArticle}/>
-      <NewsItem article={sampleArticle}/>
-      <NewsItem article={sampleArticle}/>
-      <NewsItem article={sampleArticle}/>
-      <NewsItem article={sampleArticle}/>
-      <NewsItem article={sampleArticle}/>
+      {articles.map((article)=>(
+        <NewsItem key={article.url} article={article}/>
+      ))}
     </NewsListBlock>
   );
 };
