1281d78c6860fb4947984d08890b19cf078d5837
Your Name <dnjswls0506@naver.com>
2022-05-09 00:58:44 +0900

    리액트(redux 재설치)

--- react_practice4/src/index.js
+++ react_practice4/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createStore} from 'redux';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
