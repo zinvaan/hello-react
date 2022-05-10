ca6f10be567deff6be2795af837a136c82c46ea0
zinvaan <wonjin89jung@gmail.com>
2022-05-10 15:08:09 +0900

    리액트(백엔드 REST API, 라우트 모듈화)

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,18 +1,15 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
 // Router 설정
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
-router.get('/about', (ctx) => {
-  ctx.body = '소개';
-});
+router.use('/api', api.routes()); // api route 적용
 
-// app instance에 router 적용
+// app instance에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
