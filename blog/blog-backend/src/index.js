bf732409157b92478bbf649a5dd3801b446ae382
zinvaan <wonjin89jung@gmail.com>
2022-05-10 16:36:48 +0900

    리액트(koa 프레임워크, mongoDB설치)

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 
@@ -8,6 +9,9 @@ const router = new Router();
 
 // Router 설정
 router.use('/api', api.routes()); // api route 적용
+
+// Router 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 // app instance에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
