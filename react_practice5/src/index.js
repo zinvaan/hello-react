5b9b846aef6a9f2415bce85b8754cdd263dc0715
zinvaan <wonjin89jung@gmail.com>
2022-05-10 13:46:12 +0900

    리액트(백엔드:koa프레임워크 설치)

--- react_practice5/src/index.js
+++ react_practice5/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {createStore} from 'redux';
-import { Provider } from 'react-redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./modules";
 
 const store = createStore(rootReducer, composeWithDevTools());
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
@@ -17,6 +17,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
