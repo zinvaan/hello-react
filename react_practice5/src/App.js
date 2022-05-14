a7d9653fadab8c56adee6402664aac4077f3839e
zinvaan <wonjin89jung@gmail.com>
2022-05-14 17:45:23 +0900

    리액트(복습)

--- react_practice5/src/App.js
+++ react_practice5/src/App.js
@@ -1,15 +1,12 @@
-import React from "react";
-import CounterContainer from "./containers/CounterContainer";
-import TodosContainer from "./containers/TodosContainer";
+import React from 'react';
+import Say from './Say';
 
-const App=()=>{
+const App = () => {
   return(
     <div>
-      <CounterContainer/>
-      <hr/>
-      <TodosContainer/>
+      <Say />
     </div>
-  );
+  )
 };
 
 export default App;
