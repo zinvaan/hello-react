496036bea2edb4c005eabe35bcaf5f70fdf3c946
zinvaan <wonjin89jung@gmail.com>
2022-05-18 15:00:47 +0900

    리액트(hook복습)

--- react_practice5/src/App.js
+++ react_practice5/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import IterationSample from './IterationSample';
+import React, { useState } from 'react';
+import Info from './components/Info';
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return(
     <div>
-      <IterationSample />
+      <button onClick={()=>{setVisible(!visible)}}>
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr/>
+      {visible && <Info/>}
     </div>
   )
 };
