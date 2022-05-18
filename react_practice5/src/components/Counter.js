04e4f6e57b219e599336f94f3ac461808b1e4e2f
zinvaan <wonjin89jung@gmail.com>
2022-05-18 15:17:07 +0900

    리액트(useReducer 복습1)

--- react_practice5/src/components/Counter.js
+++ react_practice5/src/components/Counter.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-const Counter=({number, onIncrease, onDecrease})=>{
-  return(
+function reducer(state, action) {
+  // action.type에 따라 다른 작업 수행
+  switch(action.type) {
+    case 'INCREASE':
+      return { value: state.value + 1 };
+    case 'DECREASE':
+      return { value: state.value - 1 };
+    default:
+      // 아무것도 해당되지 않을 때 기존 상태 반환
+      return state;
+  }
+}
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
+  return (
     <div>
-      <h1>{number}</h1>
-      <div>
-        <button onClick={onIncrease}>+1</button>
-        <button onClick={onDecrease}>-1</button>
-      </div>
+      <p>현재 카운터 값은 <b>{state.value}</b>입니다.</p>
+      <button onClick={()=>{dispatch({type: 'INCREASE'})}}>+1</button>
+      <button onClick={()=>{dispatch({type: 'DECREASE'})}}>-1</button>
     </div>
-  );
-};
+  )
+}
 
 export default Counter;
