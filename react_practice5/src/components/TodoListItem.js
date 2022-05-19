efb351b588ff6e6bbb19d1af43b5a6fc12a93bc0
zinvaan <wonjin89jung@gmail.com>
2022-05-19 16:07:38 +0900

    리액트(todo리스트 완성)

--- react_practice5/src/components/TodoListItem.js
+++ react_practice5/src/components/TodoListItem.js
@@ -5,15 +5,23 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 import './TodoListItem.scss';
+import cn from 'classnames'
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
+  const onClickRemove = () => {
+    onRemove(id);
+  };
+  const onClickToggle = () => {
+    onToggle(id);
+  }
   return (
     <div className="TodoListItme">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank/>
-        <div className="text">할 일</div>
+      <div className={cn('checkbox',{ checked })} onClick={onClickToggle}>
+        {checked ? <MdCheckBox/>:<MdCheckBoxOutlineBlank/>}
+        <div className="text">{text}</div>
       </div>
-      <div className="remove">
+      <div className="remove" onClick={onClickRemove}>
         <MdRemoveCircleOutline/>
       </div>
     </div>
