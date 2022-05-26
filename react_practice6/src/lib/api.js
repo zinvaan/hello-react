be1ded350e34400d4430859283f3c86a907c1eed
zinvaan <wonjin89jung@gmail.com>
2022-05-26 17:25:29 +0900

    리액트(api return오류수정)

--- react_practice6/src/lib/api.js
+++ react_practice6/src/lib/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const getPost = (id) => {
-  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
 };
 export const getUsers = (id) => {
-  axios.get(`https://jsonplaceholder.typicode.com/users`);
+  return axios.get(`https://jsonplaceholder.typicode.com/users`);
 };
 
 /*
