68d72cde44ec961921afc4ffdfdc38bf50feb008
zinvaan <wonjin89jung@gmail.com>
2022-06-08 22:23:31 +0900

    리액트(axios 사용하기)

--- react_practice5/src/App.js
+++ react_practice5/src/App.js
@@ -1,64 +1,31 @@
 import React, { useState } from 'react';
-import './App.css';
-
-const Modal = () => {
-  return (
-    <div className="modal">
-      <h4>제목</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  );
-};
+import axios from 'axios';
 
 const App = () => {
-  const [title, setTitle] = useState([
-    '남자 코트 추천',
-    '강남 우동맛집',
-    '파이썬 독학',
-  ]);
-  const [number, setNumber] = useState(0);
-  const onClickButton = () => {
-    setNumber(number + 1);
-  };
-  const onClickModify = () => {
-    let copy = [...title];
-    copy[0] = '여자코트 추천';
-    setTitle(copy);
-  };
-  const onClickSort = () => {
-    let copy = [...title];
-    setTitle(copy.sort());
-    console.log(title);
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      await axios
+        .get('https://jsonplaceholder.typicode.com/todos/1')
+        .then((response) => {
+          setData(response.data);
+        });
+    } catch (e) {
+      console.log('error: ', e);
+    }
   };
   return (
-    <div className="App">
-      <div className="black-nav">
-        <h4>ReactBlog</h4>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+        {data && (
+          <textarea
+            rows={7}
+            value={JSON.stringify(data, null, 2)}
+            readOnly={true}
+          />
+        )}
       </div>
-      <button onClick={onClickSort}>가나다순정렬</button>
-      <button className="nodify-button" onClick={onClickModify}>
-        수정하기
-      </button>
-      <div className="list">
-        <h4>
-          {title[0]}{' '}
-          <span className="like-button" onClick={onClickButton}>
-            👍
-          </span>
-          {number}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <Modal />
     </div>
   );
 };
