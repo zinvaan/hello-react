a1399b772371fb0bed48c19e67a423a3b86380b9
zinvaan <wonjin89jung@gmail.com>
2022-06-17 16:53:14 +0900

    리액트(callback 2)

--- react_practice5/src/Async.js
+++ react_practice5/src/Async.js
@@ -1,11 +1,28 @@
 const Async = () => {
-  const one = (call_two) => {
-    console.log(' step 1 complete. please call step 2');
-    call_two();
+  let stocks = {
+    Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ['water', 'ice'],
+    holder: ['cone', 'cup', 'stick'],
+    toppings: ['chocolate', 'peanuts'],
   };
-  const two = () => {
-    console.log(' step 2 ');
+  let order = (Fruit_index, call_production) => {
+    setTimeout(() => {
+      console.log(`${stocks.Fruits[Fruit_index]} was selected.`);
+      call_production();
+    }, 2000);
   };
-  one(two);
+  let production = () => {
+    setTimeout(() => {
+      console.log('production has started.');
+    }, 0);
+    setTimeout(() => {
+      console.log('the fruit has been chopped.');
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added.`);
+      }, 1000);
+    }, 2000);
+  };
+
+  order('0', production);
 };
 export default Async;
