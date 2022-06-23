fa6cd9225c4b67d3027ea971c07dc1200be94fe5
zinvaan <wonjin89jung@gmail.com>
2022-06-23 15:02:54 +0900

    리액트(async/await 연습)

--- react_practice5/src/Async.js
+++ react_practice5/src/Async.js
@@ -104,26 +104,41 @@ let is_shop_open = true;
 };
 */
 
-  let is_shop_open = true;
-  const toppings_choice = () => {
+  let is_shop_open = false;
+  function time(ms) {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(console.log('whitch topping would you love?'));
-      }, 3000);
+      if (is_shop_open) {
+        setTimeout(resolve, ms);
+      } else {
+        //shop closed
+        reject(console.log('shop is closed'));
+      }
     });
-  };
-
+  }
   async function kitchen() {
-    console.log('A');
-    console.log('B');
-    console.log('C');
-    await toppings_choice();
-    console.log('D');
-    console.log('E');
+    try {
+      await time(2000);
+      console.log(`${stocks.Fruits[0]} was selected.`);
+      await time(0);
+      console.log('start the production');
+      await time(2000);
+      console.log('cut the fruit');
+      await time(1000);
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added.`);
+      await time(1000);
+      console.log('start the machine;');
+      await time(2000);
+      console.log(`ice cream placed on ${stocks.holder[0]}`);
+      await time(3000);
+      console.log(`${stocks.toppings[0]} was selected.`);
+      await time(2000);
+      console.log('serve ice cream');
+    } catch (error) {
+      console.log('customer left', error);
+    } finally {
+      console.log('day ended, shop is closed.');
+    }
   }
   kitchen();
-  console.log('doing the dishes');
-  console.log('cleaning the tables');
-  console.log('taking others order');
 };
 export default Async;
