174cd7bda02b6b998fd0c688fd349525dfd5ab96
zinvaan <wonjin89jung@gmail.com>
2022-06-23 14:48:28 +0900

    리액트(async/await 동작)

--- react_practice5/src/Async.js
+++ react_practice5/src/Async.js
@@ -105,33 +105,25 @@ let is_shop_open = true;
 */
 
   let is_shop_open = true;
+  const toppings_choice = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(console.log('whitch topping would you love?'));
+      }, 3000);
+    });
+  };
 
-  // const order = () => {
-  //   return new Promise((resolve, reject) => {
-  //     if(true){
-  //       resolve()
-  //     }
-  //     else{
-  //       reject()
-  //     }
-  //   });
-  // };
-
-  // order( )
-  // .then()
-  // .then()
-  // .then()
-  // .catch()
-  // .finally()
-
-  async function order() {
-    try {
-      await abc;
-    } catch (error) {
-      console.log("abc doesn't exist", error);
-    } finally {
-      console.log('runs code anyways');
-    }
+  async function kitchen() {
+    console.log('A');
+    console.log('B');
+    console.log('C');
+    await toppings_choice();
+    console.log('D');
+    console.log('E');
   }
+  kitchen();
+  console.log('doing the dishes');
+  console.log('cleaning the tables');
+  console.log('taking others order');
 };
 export default Async;
